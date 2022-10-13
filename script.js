4a2f2ac4b3917058a390e734b8a748b3954b902b
Evan Curtis <ewcurtis1997@gmail.com>
2022-10-13 17:45:51 -0400

    Restore player vs ai functionality and ability to transition between gameboard and start screen

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const message = document.querySelector(".message");
 const cells = document.querySelectorAll(".card");
-
+const gameBoard = document.querySelector(".gameboard");
+const gameMode = document.querySelector(".gamemode");
 
 const game = (() => {
-    const gameBoard = document.querySelector(".gameMode");
-    const gameMode = document.querySelector(".gameMode");
-    const player1 = document.querySelector("#player1");
-    const player2 = document.querySelector("#player2");
     const pvpButton = document.querySelector(".pvp");
-    const aiButton = document.querySelector("ai");
+    const aiButton = document.querySelector(".ai");
 
-    let arr = ["","","","","","","","",""];
-    const checkGameOver = () => {
+    const checkGameOver = (arr) => {
         let gameOver = false;
         let tie = false;
+        console.log("checkGameOver: " + arr);
         if ((arr[0] === arr[1] && arr[0] === arr[2] && arr[0] !== "") || (arr[3] === arr[4] && arr[3] === arr[5] && arr[3] !== "") ||
         (arr[6] === arr[7] && arr[6] === arr[8] && arr[6] !== "") || (arr[0] === arr[3] && arr[0] === arr[6] && arr[0] !== "") ||
         (arr[1] === arr[4] && arr[1] === arr[7] && arr[1] !== "") || (arr[2] === arr[5] && arr[2] === arr[8] && arr[2] !== "") ||
         (arr[0] === arr[4] && arr[0] === arr[8] && arr[0] !== "") || (arr[2] === arr[4] && arr[2] === arr[6] && arr[2] !== "")) {
             gameOver = true;
-        } else {
+        } else {          
             for (let i = 0; i < arr.length; i++) {
+                console.log(`i: ${i}`);
                 if (arr[i] === "") break;
                 if (i === 8) {
                     if (arr[i] !== "") {
@@ -30,43 +28,47 @@ const game = (() => {
                 }
             }
         }
-        return {arr, gameover, tie};
+        return {gameOver, tie};
         //playerOne = !playerOne;
     };
 
-    const pcTurn = () => {
+    //Sets AI
+    const pcTurn = (arr) => {
         let guess = Math.floor(Math.random() * 8);
         console.log(guess);
         if (cells[guess].textContent === "") {
             cells[guess].textContent = "O"; 
             arr[guess] = "O";
         } else {
-            pcTurn();
+            pcTurn(arr);
         }
        
     }
 
-    const clearGameBoard = () => {
-        cells.forEach(cell => {
-            cell.textContent = "";
-        })
-        game.arr = ["","","","","","","","",""];
-        message.textContent = "";
-    }
+    //PVP game mode
+    pvpButton.addEventListener("click", e => {
 
-    const reset = document.querySelector(".reset");
-    reset.addEventListener("click", e => {
-        clearGameBoard();
+        const player1 = document.querySelector("#player1");
+        const player2 = document.querySelector("#player2");
+        if (player1.checkValidity() && player2.checkValidity() && player1.value !== player2.value) {
+            let p1 = playerFactory(player1.value);
+            let p2 = playerFactory(player2.value);
+            newGameFactory(true, p1, p2);
+            gameBoard.style.cssText = "display: grid;";
+            gameMode.style.cssText = "display: none";
+        } else {
+            player1.reportValidity();
+            player2.reportValidity();
+        }
     });
-    //New Game - Takes player back to initial screen
-    const ng = document.querySelector(".new-game");
-    ng.addEventListener("click", e => {
-        clearGameBoard();
-        gameBoard.style.cssText = "display: none;";
-        gameMode.style.cssText = "display: grid";
+
+    aiButton.addEventListener("click", e => {
+        gameBoard.style.cssText = "display: grid;";
+        gameMode.style.cssText = "display: none";
+        newGameFactory(false,"","");
     })
 
-    return {arr, checkGameOver, pcTurn};
+    return {checkGameOver, pcTurn};
 })();
 
 
@@ -76,32 +78,29 @@ const playerFactory = name => {
 };
 
 //Games
-const newGameFactory = gamemode => {
+const newGameFactory = (gamemode, p1, p2) => {
 
     let playerOne = true;
-    let p1;
-    let p2;
-
-    if (gameMode === "pvp") {
-        p1 = playerFactory(document.querySelector("#player1").value);
-        p2 = playerFactory(document.querySelector("#player2").value);
-
-    }
+    let arr = ["","","","","","","","",""];
 
     cells.forEach(cell => {
         cell.addEventListener("click", e => {
             if (message.textContent === "" && cell.textContent === "") {
-                if (gameMode !== "pvp") {
+                if (!gamemode) {
                     //vs AI
                     cell.textContent = "X";
-                    game.arr[parseInt(cell.getAttribute("id"))] = "X";
-                    let gameCheck = game.checkGameOver();
+                    console.log("got here!");
+                    arr[parseInt(cell.getAttribute("id"))] = "X";
+                    console.log("Got here too!");
+                    console.log("cell: " + arr[parseInt(cell.getAttribute("id"))]);
+                    let gameCheck = game.checkGameOver(arr);
+                    console.log(gameCheck.gameOver);
     
                     if (!gameCheck.gameOver) {
                         playerOne = false;
-                        game.pcTurn();
-                        gameCheck = game.checkGameOver();
-                        playerOne = true;
+                        game.pcTurn(arr);
+                        gameCheck = game.checkGameOver(arr);
+                        
                     }
     
                     if (gameCheck.gameOver) {
@@ -114,24 +113,25 @@ const newGameFactory = gamemode => {
                             message.textContent = "You lose!";
                         }
                     }
-                    playerOne = true;
+                   
                 }
+                playerOne = true;
     
                 } else {
                     //PVP
                     if (playerOne) {
                     cell.textContent = "X";
-                    game.arr[parseInt(cell.getAttribute("id"))] = "X";
+                    arr[parseInt(cell.getAttribute("id"))] = "X";
                     
                     } else {
                         cell.textContent = "O";
-                    game.arr[parseInt(cell.getAttribute("id"))] = "O";
+                    arr[parseInt(cell.getAttribute("id"))] = "O";
 
                     }
 
                     playerOne = !playerOne;
     
-                    let gameCheck = checkGameOver();
+                    let gameCheck = game.checkGameOver();
                     if (gameCheck.gameOver) {
                         if (gameCheck.tie) {
                             message.textContent = "It's a tie!"
@@ -142,10 +142,33 @@ const newGameFactory = gamemode => {
                             message.textContent = `${p2.name} wins!`;
                         }
                     }
-                        playerOne = true;
+                        
                     }
                 }  
             }
         })
+    });
+
+    const clearGameBoard = () => {
+        cells.forEach(cell => {
+            cell.textContent = "";
+        });
+        arr = ["","","","","","","","",""];
+        message.textContent = "";
+        playerOne = true;
+    };
+
+
+    //Reset Game with initial settings
+    const reset = document.querySelector(".reset");
+    reset.addEventListener("click", e => {
+        clearGameBoard();
+    });
+    //New Game - Takes player back to initial screen
+    const ng = document.querySelector(".new-game");
+    ng.addEventListener("click", e => {
+        clearGameBoard();
+        gameBoard.style.cssText = "display: none;";
+        gameMode.style.cssText = "display: grid";
     });
 };
